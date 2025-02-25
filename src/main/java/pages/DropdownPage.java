4242685e21ab1b34db8f97566b1944947c178f52
ladyemilyy <equarsh33@gmail.com>
2025-02-25 14:28:50 +0000

    New test class for dropdown lists

--- src/main/java/pages/DropdownPage.java
+++ src/main/java/pages/DropdownPage.java
@@ -1,18 +1,37 @@
 package pages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class DropdownPage {
 
     //fields
     private WebDriver driver;
+    private By dropdown = By.id("dropdown");
 
     //constructor
     public DropdownPage (WebDriver driver){
         this.driver = driver;
     }
 
-    public void selectDropdown(){
+    public void selectFromDropdown(String option){
+        findDropdownELement().selectByVisibleText(option);
+    }
 
+    public List<String> getSelectedOption(){
+        List<WebElement> selectedElements =
+            findDropdownELement().getAllSelectedOptions();
+        return selectedElements.stream().map(WebElement::getText).collect(Collectors.toList());
+    }
+
+    public Select findDropdownELement(){
+        return new Select(driver.findElement(dropdown));
     }
 }
+
+
--- src/test/java/Interactions/InteractionTests.java
+++ src/test/java/Interactions/InteractionTests.java
@@ -1,6 +1,20 @@
 package Interactions;
 
-public class InteractionTests {
+import base.BaseTests;
+import org.testng.annotations.Test;
 
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
+
+public class InteractionTests extends BaseTests {
+
+    @Test
+    public void TestDropDdwn(){
+        var dropDownpage = homepage.clickDropdownLink();
+        dropDownpage.selectFromDropdown("Option 1");
+        var selectedOption =dropDownpage.getSelectedOption();
+        assertEquals(selectedOption.size(), 1, "Incorrect number of selections");
+        assertTrue(selectedOption.contains("Option 1"), "Option not selected");
+    }
 
 }
